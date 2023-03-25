21def8c02927c9dd8a47e626cfbf82a65c96a607
IrynaBuchma <buchma.iryna2018@gmail.com>
2023-03-25 10:41:49 +0200

    write fetch-countries code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './js/fetch-countries';
+
 
 const DEBOUNCE_DELAY = 300;
+
+const countryInput = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+countryInput.addEventListener('input', debounce(onCountryInput, DEBOUNCE_DELAY));
+
+function onCountryInput() {
+    const name = countryInput.value.trim();
+    if (name === '') {
+        return (countryList.innerHTML = ''), (countryInfo.innerHTML = '');
+    }
+}
 
